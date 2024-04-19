66ce65aa395fe1305c06ae2a91fe6a02cb7d7db9
Atake <146906117+Atake2525@users.noreply.github.com>
2024-04-19 10:37:23 +0900

    01-02確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,11 +2,18 @@
 #include "TextureManager.h"
 #include <cassert>
 
-GameScene::GameScene() {
-	delete sprite_;
-}
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
 
-GameScene::~GameScene() {}
+GameScene::GameScene() {}
+
+
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
@@ -15,10 +22,65 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 
 	textureHandle_ = TextureManager::Load("inshipPlayer_front_1.png");
+	model_ = Model::Create();
+
+	//スプライト
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	//サウンドデータ読み込み
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	//音声再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	//軸方向表示の表示を有効化する
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//スプライトの今の座標を取得
+	Vector2 pos = sprite_->GetPosition();
+	//座標を{ 2, 1 }移動
+	pos.x += 2.0f;
+	pos.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(pos);
+
+	//スペースを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		//音声停止
+		audio_->StopWave(soundDataHandle_);
+	}
+
+	#ifdef _DEBUG
+	//デバッグテキストの表示
+	ImGui::Begin("Debug1");
+	//float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	//float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+	//デモウィンドウの表示を有効化
+	ImGui::ShowDemoWindow();
+
+	#endif
+
+	//デバッグカメラの更新
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -47,6 +109,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -62,6 +126,10 @@ void GameScene::Draw() {
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
+
+	//ラインを描画する
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
+
 
 #pragma endregion
 }
